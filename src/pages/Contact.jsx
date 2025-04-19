67846202211ed713808b75fbc72cf1e8ecdbf55a
Jeff B <Jeff.BozierJr@gmail.com>
2025-04-19 09:42:51 -0400

    contact css fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import '../styles/Contact.css'
+import '../styles/Contact.css';
 
 const Contact = () => {
   return (
-    <section className="min-h-screen bg-white text-gray-900 py-16 px-4 sm:px-6 lg:px-8">
+    <section className="min-h-screen py-16 px-4 sm:px-6 lg:px-8">
     <div className="max-w-3xl mx-auto text-center">
-      <h1 className="text-4xl font-bold mb-4">Let’s Talk</h1>
-      <p className="text-lg text-gray-600 mb-12">
+      <h1 className="text-4xl font-bold mb-4 contactTitle">Let’s Talk</h1>
+      <p className="contactText text-lg mb-12">
         Send all inquiries and questions through here.
       </p>
     </div>
