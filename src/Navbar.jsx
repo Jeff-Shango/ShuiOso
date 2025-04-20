f0fb7317dc14fbb96e17efe62d46f85a60af43cd
Jeff B <Jeff.BozierJr@gmail.com>
2025-04-20 10:45:32 -0400

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './styles/Navbar.css';
 import logoImage from './assets/photos/ShuiOsoJLogo.png'; // update path as needed
-import MusicPlayer from './pages/MusicPlayer';
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
